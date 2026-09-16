1c341cc136fbe861d7ea4cfc017c25c4a6fc4cfd
noorapp10000-web <noor.app.10000@gmail.com>
2026-09-16 04:45:35 +0300

    Set minSdk to 23 in build.gradle.kts

--- flutter_app/android/app/build.gradle.kts
+++ flutter_app/android/app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         applicationId = "com.noorapp.egyptian_football_hub"
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
